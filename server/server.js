9b2c52d70ddc5b3b7b9079e2a267ec145a8ee12a
Michael Harris <mwharris333@gmail.com>
2015-05-13 10:51:28 -0700

    Create data array from image file

--- server/server.js
+++ server/server.js
@@ -1,34 +1,24 @@
-var dvv = require('./dvv.js');
+imageUtils = require('./imageUtils.js');
 
-//Function to create arrays for testing purposes
-var createMatrixArrays = function(matrixSize, arrayLength){
-  var randomArray = function(n){
-    var result = [];
-    for(var i = 0; i < n ; i++){
-      var row = [];
-      for (var j = 0; j < n; j++){
-        row.push(Math.floor(Math.random()*100));
-      }
-      result.push(row);
-    }
-    return result;
-  };
+var init = function(dataArray){
+  console.log('DATA ARRAY:')
+  console.log(dataArray);
 
-  var result = [];
-  for(var i = 0; i < arrayLength; i++){
-    result.push(randomArray(matrixSize));
-  }
-  return result;
+  var dvv = require('./dvv.js');
+
+  // //Test : invert a matrix using math.js's math.inv function imported on client side
+  // //partitionLength is set to 1 by default
+  dvv.config({
+   staticPath: '/../client',
+   timeout: 25000,
+   // data: createMatrixArrays(200, 5),
+   data: dataArray,
+   func: 'math.inv',
+   clock: true
+  });
+
+  dvv.start();
+
 };
 
-// //Test : invert a matrix using math.js's math.inv function imported on client side
-// //partitionLength is set to 1 by default
-dvv.config({
- staticPath: '/../client',
- timeout: 25000,
- data: createMatrixArrays(200, 5),
- func: 'math.inv',
- clock: true
-});
-
-dvv.start();
+imageUtils.createDataArrayFromImageFile('server/rgb.png', init);
